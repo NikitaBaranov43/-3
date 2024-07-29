0302c6f18f9d072350c2ca69ee740c016a13edcd
NikitaBaranov43 <fast.flou@gmail.com>
2024-07-29 18:23:23 +0300

    Add files via upload

---
+++ ДЗ3.py
@@ -0,0 +1,4 @@
+number_of_completed_HW = 12
+number_of_hours_spent = 1.5
+course_name = 'Python,'
+print (course_name, "всего задач:12", 'затрачено часов:', number_of_hours_spent , ', среднее время выполнения ' ,number_of_hours_spent / number_of_completed_HW ,"часов")
